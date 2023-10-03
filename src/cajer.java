fcfa47ea4f9f1fd28803e0fef95a51242eff7ef3
Yesica Beltran <guzman.carvajal240406@gmail.com>
2023-10-03 16:49:18 -0500

    Cajeroautomat

--- src/cajer.java
+++ src/cajer.java
@@ -1,47 +1,55 @@
 import javax.swing.*;
+import java.awt.desktop.SystemSleepEvent;
 
 public class cajer {
 
         public static void main(String [] args) {
-            int opcion;
-            final int saldo_inicial = 1000;
-            Float ingreso, saldo;
+            int opcion = 0;
+            int saldo = 1000;
+            Float ingreso;
             Float retiro;
+            while (opcion != 3) {
+                opcion = Integer.parseInt(JOptionPane.showInputDialog(null, "Hola bienvenido al cajero automatico \n" +
+                        " 1 . Porfavor ingrese el dinero a la cuenta \n" +
+                        " 2 . retiro del dinero de la cuenta \n" +
+                        " 3 . desea salir \n" +
+                        "============================================================================="));
 
-            opcion = Integer.parseInt(JOptionPane.showInputDialog(null, "Hola bienvenido al cajero automatico \n" +
-                    " 1 . Porfavor ingrese el dinero a la cuenta \n" +
-                    " 2 . retiro del dinero de la cuenta \n" +
-                    " 3 . desea salir \n" +
-                    "============================================================================="));
+                switch (opcion) {
+                    case 1:
+                        ingreso = Float.parseFloat(JOptionPane.showInputDialog("por favor ingrese la cantidad que quiere a la cuenta: "));
+                        saldo = (int) (saldo + ingreso);
+                        JOptionPane.showMessageDialog(null, " dienro en la cuenta : " + saldo);
+                        break;
 
-            switch (opcion) {
-                case 1:
-                    ingreso = Float.parseFloat(JOptionPane.showInputDialog("por favor ingrese la cantidad que quiere a la cuenta: "));
+                    case 2:
+                        retiro = Float.parseFloat(JOptionPane.showInputDialog("ingrese la cantidad  que quieres retirar:"));
 
-                    saldo = saldo_inicial + ingreso;
-                    JOptionPane.showMessageDialog(null," dienro en la cuenta : " + saldo);
-                    break;
+                        if (retiro > saldo) {
+                            JOptionPane.showInputDialog(null, "Ojo no tenes saldo suficiente para retirar su plata");
 
-                case 2:
-                    retiro = Float.parseFloat(JOptionPane.showInputDialog("ingrese la cantidad  que quieres retirar:"));
+                        }
+                        else {
+                            saldo = (int) (saldo - retiro);
+                            JOptionPane.showMessageDialog(null, "Dinero en la cuenta : " + saldo);
 
-                    if (retiro > saldo_inicial) {
-                        JOptionPane.showInputDialog(null, "Ojo no tenes saldo suficiente para retirar su plata");
 
-                    } else {
-                        saldo = saldo_inicial - retiro;
-                        JOptionPane.showMessageDialog(null, "Dinero en la cuenta : " + saldo);}
-                    break;
-                case 3:
-                    JOptionPane.showMessageDialog(null, "JAJAJA te equivocaste machi de opcion  de menu");
+                            break;
 
+
+                        }   default:
+                                JOptionPane.showMessageDialog(null, "salir del siste :)");
+                                break;
+                }
             }
         }
+}
 
 
 
 
-    }
+
+
 
 
 
